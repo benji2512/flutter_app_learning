a4c7c94d2dacb6c6d7b84b7b6499635f3638d3eb
Ben Clarke <benthedev@outlook.com>
2021-05-27 18:13:23 +0100

    Finished part 2 and built an apk for phone

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() =>  runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     //final wordPair = WordPair.random();
     return MaterialApp(
-      //title: "Welcome to Flutter",
-      title: "Startup Name Generator",
+      title: "BenTheDev's Name Generator",
+      theme: ThemeData(primaryColor: Colors.red[700]),
       home: RandomWords(),
-      //home: Scaffold(
-        //appBar: AppBar(
-          //title: Text("Welcome to Flutter")
-        
-        //),
-        //body: Center(
-          //child: Text("Hello World"),
-          //child: Text(wordPair.asPascalCase),
-          //child: RandomWords(),
-        //),
-      //),
+      // Below commented lines are an example of how to add background image to Home screen of app
+      // Container(
+      //   decoration: BoxDecoration(
+      //       image: DecorationImage(
+      //     image: AssetImage("media/logoTransparent.png"),
+      //     fit: BoxFit.cover,
+      //   )),
+      //   child: RandomWords(),
+      // ));
     );
   }
 }
@@ -33,35 +31,84 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _biggerFont = TextStyle(fontSize: 18.0);
+  final _saved = <WordPair>{};
+  final _biggerFont = TextStyle(fontSize: 25.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Startup Name Generator"),
-    ),
-    body: _buildSuggestions(),
+        title: Text("BenTheDev's Name Generator"),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
     );
   }
-  Widget _buildSuggestions(){
-    return ListView.builder(
-      padding: EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
-        if (i.isOdd) return Divider();
 
-        final index = i ~/ 2;
-        if (index >= _suggestions.length){
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      });
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+              : <Widget>[];
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Saved Suggestions"),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
-  Widget _buildRow(WordPair pair){
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(15.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
